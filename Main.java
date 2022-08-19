8d88a936259c4884645086b9b8720daba4310ee1
Davle8 <baza020sb@yandex.ru>
2022-08-19 14:50:24 +0500

    fourth question 2/2

--- Main.java
+++ Main.java
@@ -2,9 +2,19 @@ package homework.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = {new Cat("Bars", 200, 0), new Dog("Alpha", 500, 10)};
-        for ( Animal o : animals ) {
+        Animal[] animals = {
+                new Cat("Bars", 200, 0),
+                new Cat("Kotyara", 300, 2),
+                new Cat("Many", 150, 0),
+                new Dog("Alpha", 500, 10),
+                new Dog("Beta", 400, 11),
+                new Dog("Gamma", 600, 8),
+
+        };
+        for (Animal o : animals) {
             o.running();
+        }
+        for (Animal o : animals) {
             o.swimming();
         }
     }
